86ae6cb25cba02f0c67744688387652ddb1e1067
mayankpande88 <32476957+mayankpande88@users.noreply.github.com>
2019-05-19 18:53:36 +0530

    added code mirror as text area

--- src/component/card-buttons/CardButtons.js
+++ src/component/card-buttons/CardButtons.js
@@ -4,10 +4,10 @@ function CardButtons({buttonClick}) {
   return (
     <div>
 		<div className="flex flex-column">
-			<button className="tc grow pa3 ma2  bw2 mt6" onClick={buttonClick}>XML</button>
-			<button className="tc grow pa3 ma2  bw2 " onClick={buttonClick}>JSON</button>
-				<button className="tc grow  pa3 ma2  bw2 " onClick={buttonClick}>SQL</button>
-			<button className="tc grow pa3 ma2  bw2 " onClick={buttonClick}>RESET</button>
+			<button className="tc grow pa3 ma2  bw2 mt6 button" onClick={buttonClick}>XML</button>
+			<button className="tc grow pa3 ma2  bw2 button" button={buttonClick}>JSON</button>
+				<button className="tc grow  pa3 ma2  button " onClick={buttonClick}>SQL</button>
+			<button className="tc grow pa3 ma2  bw2 button" onClick={buttonClick}>RESET</button>
 		</div>
     </div>
   );
